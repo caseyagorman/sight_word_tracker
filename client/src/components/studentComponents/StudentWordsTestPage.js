77d0ea0a51dae6cb9d2d03a4fd285ca47e6ec5cc
Casey Gorman <caseyagorman@gmail.com>
2018-11-30 21:12:03 -0800

    toggle visbility on each word with separate state

--- client/src/components/studentComponents/StudentWordsTestPage.js
+++ client/src/components/studentComponents/StudentWordsTestPage.js
@@ -1,30 +1,15 @@
 import React from "react";
+import DisplayWord from "./DisplayWord";
 
 class StudentWordsTestPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { words: this.props.words, showing: true };
+    this.state = { words: this.props.words };
   }
-  displayTest(word) {
-    if (!word) {
-      return <div>loading...</div>;
-    }
-    console.log(word);
-    const showing = word.showing;
-    return (
-      <div>
-        {showing ? <div>{word}</div> : null}
-        <button onClick={() => this.setState({ showing: !showing })}>
-          toggle
-        </button>
-      </div>
-    );
-  }
-  render() {
-    const words = this.state.words;
-    return words.map(word => this.displayTest(word));
 
-    // return <div> {this.state.words.map(word => this.DisplayTest(word))} </div>;
+  render() {
+    const words = this.props.words;
+    return words.map(word => <DisplayWord word={word} />);
   }
 }
 
