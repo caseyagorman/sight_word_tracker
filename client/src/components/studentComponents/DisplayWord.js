7a8a4a3cdaf36c384227fbc876c82c8a612f4fbf
Casey Gorman <caseyagorman@gmail.com>
2018-12-01 12:45:58 -0800

    test words progress

--- client/src/components/studentComponents/DisplayWord.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DisplayWord = props => {
-  return <div>{props}</div>;
-};
-
-export default DisplayWord;
--- client/src/components/studentComponents/StudentWordsTestPage.js
+++ client/src/components/studentComponents/StudentWordsTestPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import DisplayWord from "./DisplayWord";
 
 class StudentWordsTestPage extends React.Component {
   constructor(props) {
@@ -20,18 +19,29 @@ class StudentWordsTestPage extends React.Component {
     console.log("yes!");
   }
 
+  stub(e, word) {
+    e.preventDefault();
+    console.log(word, e.target.value);
+  }
   render() {
     const words = this.state.words;
     const idx = this.state.idx;
     return (
       <div>
         <div>{this.displayWord(words[idx])}</div>
-        <button onClick={() => this.incrementIdx(idx)} />
+        <button onClick={e => this.stub(e, words[idx])} value="yes">
+          Yes
+        </button>
+        <button onClick={e => this.stub(e, words[idx])} value="no">
+          No
+        </button>
+        <br />
+        <div>
+          <br />
+          <button onClick={() => this.incrementIdx(idx)}>Next</button>
+        </div>
       </div>
     );
-    {
-      /* <button onClick={(idx += 1)} /> */
-    }
   }
 }
 
