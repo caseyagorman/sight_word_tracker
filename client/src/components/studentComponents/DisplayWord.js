15689afd64f5aecd6249f9b8e55f3a96998bfe8b
Casey Gorman <caseyagorman@gmail.com>
2018-12-01 12:30:17 -0800

    changing words onclick

--- client/src/components/studentComponents/DisplayWord.js
+++ client/src/components/studentComponents/DisplayWord.js
@@ -1,27 +1,7 @@
 import React from "react";
 
-class DisplayWord extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isHidden: true
-    };
-  }
-  toggleHidden() {
-    this.setState({
-      isHidden: !this.state.isHidden
-    });
-  }
-
-  render() {
-    const { word } = this.props;
-    return (
-      <div>
-        <button onClick={() => this.toggleHidden()}>word</button>
-        {this.state.isHidden && word}
-      </div>
-    );
-  }
-}
+const DisplayWord = props => {
+  return <div>{props}</div>;
+};
 
 export default DisplayWord;
--- client/src/components/studentComponents/StudentWordsTestPage.js
+++ client/src/components/studentComponents/StudentWordsTestPage.js
@@ -4,12 +4,34 @@ import DisplayWord from "./DisplayWord";
 class StudentWordsTestPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { words: this.props.words };
+    this.state = { words: this.props.words, idx: 0 };
+  }
+
+  displayWord(words) {
+    if (!words) {
+      return <div>loading...</div>;
+    }
+    return <div>{words}</div>;
+  }
+
+  incrementIdx(idx) {
+    let new_idx = idx + 1;
+    this.setState({ idx: new_idx });
+    console.log("yes!");
   }
 
   render() {
-    const words = this.props.words;
-    return words.map(word => <DisplayWord word={word} />);
+    const words = this.state.words;
+    const idx = this.state.idx;
+    return (
+      <div>
+        <div>{this.displayWord(words[idx])}</div>
+        <button onClick={() => this.incrementIdx(idx)} />
+      </div>
+    );
+    {
+      /* <button onClick={(idx += 1)} /> */
+    }
   }
 }
 
