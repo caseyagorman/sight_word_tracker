32a753d208a2b05100a2aca817e4f0157a712566
Casey Gorman <caseyagorman@gmail.com>
2018-12-23 14:13:57 -0800

    add delete student form container

---
+++ client/src/containers/DeleteStudentFormContainer.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as authActions from "../redux/actions/authActions";
+import DeleteStudent from "../components/StudentComponents/Forms/DeleteStudent";
+class DeleteStudentFormContainer extends Component {
+  componentDidMount() {
+    console.log(this.props);
+    if (sessionStorage.length > 0) {
+      this.props.authActions.checkUser(sessionStorage);
+    } else {
+      alert("Please log in");
+      this.props.history.push("/login");
+    }
+  }
+
+  displayDeleteStudentForm() {
+    if (this.props.auth.isAuthenticated === false) {
+      return <div>loading...</div>;
+    }
+    return (
+      <div>
+        <DeleteStudent
+          userId={this.props.auth.user.userId}
+          username={this.props.auth.user.username}
+          studentId={this.props.studentId}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return <div>{this.displayDeleteStudentForm()}</div>;
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    authActions: bindActionCreators(authActions, dispatch)
+  };
+}
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DeleteStudentFormContainer);
