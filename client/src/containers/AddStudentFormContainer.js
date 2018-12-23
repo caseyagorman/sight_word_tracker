e05233fde61a981394feb49abfa753d55d98e220
Casey Gorman <caseyagorman@gmail.com>
2018-12-22 23:16:06 -0800

    add addstudentformcontainer file

---
+++ client/src/containers/AddStudentFormContainer.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as authActions from "../redux/actions/authActions";
+import AddStudent from "../components/studentComponents/Forms/AddStudent";
+class AddStudentFormContainer extends Component {
+  componentDidMount() {
+    console.log("add student form container", this.props);
+    if (sessionStorage.length > 0) {
+      this.props.authActions.checkUser(sessionStorage);
+    } else {
+      alert("Please log in");
+      this.props.history.push("/login");
+    }
+  }
+
+  displayAddStudentForm() {
+    if (this.props.auth.isAuthenticated === false) {
+      return <div>loading...</div>;
+    }
+    return (
+      <div>
+        <AddStudent
+          userId={this.props.auth.user.userId}
+          username={this.props.auth.user.username}
+          id={this.props.match.params.id}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return <div>{this.displayAddStudentForm()}</div>;
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    authActions: bindActionCreators(authActions, dispatch)
+  };
+}
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddStudentFormContainer);
