564fc05b589afa4d18e9f8ff40fddff4cfa4a1d0
Casey Gorman <caseyagorman@gmail.com>
2019-01-03 20:28:12 -0800

    add form folder

---
+++ client/src/components/studentComponents/Forms/Student/AddStudent.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as studentActions from "../../../../redux/actions/studentActions";
+import * as authActions from "../../../../redux/actions/authActions";
+import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
+
+class AddStudent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { fname: "", lname: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    if (sessionStorage.token) {
+      this.props.authActions.checkUser(sessionStorage);
+    } else {
+      alert("Please log in");
+      this.props.history.push("/login");
+    }
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    let user = this.props.token;
+    let newStudent = {
+      fname: this.state.fname,
+      lname: this.state.lname
+    };
+
+    this.props.studentActions.addStudent(newStudent, user);
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <div className="container">
+          <Label>
+            <h4>Add New Student</h4>
+          </Label>
+          <Row form>
+            <FormGroup>
+              <Col md={2}>
+                <Label>First name</Label>
+                <Input
+                  name="fname"
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </Col>
+              <Col md={2}>
+                <Label>Last name</Label>
+                <Input
+                  name="lname"
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </Col>
+            </FormGroup>
+          </Row>
+        </div>
+        <br />
+        <div className="container">
+          <Col md={4}>
+            <Button color="primary">Add student</Button>
+          </Col>
+        </div>
+      </Form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    student: state.student,
+    auth: state.auth
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    studentActions: bindActionCreators(studentActions, dispatch),
+    authActions: bindActionCreators(authActions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddStudent);
---
+++ client/src/components/studentComponents/Forms/Word/AddStudentWordForm.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as studentWordsActions from "../../../../redux/actions/studentWordsActions";
+import * as unknownWordsActions from "../../../../redux/actions/unknownWordsActions";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+const divStyle = { fontSize: "24px" };
+class AddStudentWordForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: [] };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.getOptions = this.getOptions.bind(this);
+  }
+
+  componentDidMount() {
+    const id = this.props.student[0].student_id;
+    const user = this.props.auth.user.token;
+    this.props.unknownWordsActions.fetchUnknownWords(id, user);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let newStudentWords = {
+      student: this.props.student[0].student_id,
+      words: this.state.value
+    };
+
+    let user = this.props.auth.user.token;
+    this.props.studentWordsActions.addStudentWords(newStudentWords, user);
+    this.forceUpdate();
+  }
+
+  handleChange(e) {
+    const options = e.target.options;
+    let value = [];
+    for (let i = 0, l = options.length; i < l; i++) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    this.setState({ value: value });
+  }
+
+  turnIntoArray(obj) {
+    if (!obj) {
+      return <p>Loading...</p>;
+    }
+    let wordList = [];
+    for (let key in obj) {
+      let wordObj = obj[key];
+      wordList.push(wordObj.word);
+    }
+    return wordList;
+  }
+
+  getOptions() {
+    if (!this.props.unknownWords) {
+      return <div>Loading!</div>;
+    }
+    let wordList = this.turnIntoArray(this.props.unknownWords);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <FormGroup controlId="formControlsSelectMultiple">
+          <ControlLabel>
+            <h3>Add Word to {this.props.student[0].fname}</h3>
+          </ControlLabel>
+          <strong>
+            <FormControl
+              style={divStyle}
+              componentClass="select"
+              multiple
+              value={this.state.value}
+              onChange={this.handleChange}
+            >
+              {wordList.map(word => (
+                <option key={word}>{word}</option>
+              ))}
+            </FormControl>
+          </strong>
+        </FormGroup>
+        <button className="btn btn-primary btn-md" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+
+  render() {
+    return <div>{this.getOptions()}</div>;
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    unknownWordsActions: bindActionCreators(unknownWordsActions, dispatch),
+    studentWordsActions: bindActionCreators(studentWordsActions, dispatch)
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    unknownWords: state.unknownWords,
+    studentWords: state.studentWords,
+    auth: state.auth
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddStudentWordForm);
