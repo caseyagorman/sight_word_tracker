2789e18bf9cb6facd52b5beaed0924dac9345860
Casey Gorman <caseyagorman@gmail.com>
2019-01-09 12:17:50 -0800

    update page with add sound to student

--- client/src/redux/actions/studentSoundsActions.js
+++ client/src/redux/actions/studentSoundsActions.js
@@ -3,6 +3,10 @@ function studentSounds() {
   return "http://localhost:5000/api/get-learned-words";
 }
 
+function getStudentApi(id) {
+  return `http://localhost:5000/api/details/${id}`;
+}
+
 function addStudentSoundsApi() {
   return "http://localhost:5000/api/add-sound-to-student";
 }
@@ -18,7 +22,9 @@ export function addStudentSounds(studentSounds, user) {
         "x-access-token": user
       },
       body: JSON.stringify(studentSounds)
-    });
+    })
+      .then(() => dispatch(fetchStudent(studentSounds.student, user)))
+      .then(() => dispatch(fetchUnknownSounds(studentSounds.student, user)));
   };
 }
 
@@ -39,5 +45,52 @@ export function fetchStudentSounds(id, user) {
     })
       .then(response => response.json())
       .then(studentSounds => dispatch(receiveStudentSounds(studentSounds)));
+  };
+}
+
+function getUnknownSoundsApi(id) {
+  return `http://localhost:5000/api/unknown-sounds/${id}`;
+}
+
+export function receiveUnknownSounds(unknownSounds) {
+  return {
+    type: types.RECEIVE_UNKNOWN_SOUNDS,
+    unknownSounds: unknownSounds
+  };
+}
+
+export function fetchUnknownSounds(student, user) {
+  return dispatch => {
+    return fetch(getUnknownSoundsApi(student), {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": user
+      }
+    })
+      .then(response => response.json())
+      .then(unknownSounds => dispatch(receiveUnknownSounds(unknownSounds)));
+  };
+}
+export function receiveStudent(student) {
+  console.log("receive student", student);
+  return { type: types.RECEIVE_STUDENT, student: student };
+}
+
+export function fetchStudent(student, user) {
+  return dispatch => {
+    return fetch(getStudentApi(student), {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": user
+      }
+    })
+      .then(response => response.json())
+      .then(student => dispatch(receiveStudent(student)));
   };
 }
